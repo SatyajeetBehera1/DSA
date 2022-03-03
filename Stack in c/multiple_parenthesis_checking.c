e5f82860e3794709abdce3ad4093432ecda05e30
SatyajeetBehera1 <86057919+SatyajeetBehera1@users.noreply.github.com>
2022-03-03 21:56:54 +0530

    Update multiple_parenthesis_checking.c

--- Stack in c/multiple_parenthesis_checking.c
+++ Stack in c/multiple_parenthesis_checking.c
@@ -1,13 +1,14 @@
+
 #include <stdio.h>
 #include <stdlib.h>
-
+ 
 struct stack
 {
     int size;
     int top;
     char *arr;
 };
-
+ 
 int isEmpty(struct stack *ptr)
 {
     if (ptr->top == -1)
@@ -19,7 +20,7 @@ int isEmpty(struct stack *ptr)
         return 0;
     }
 }
-
+ 
 int isFull(struct stack *ptr)
 {
     if (ptr->top == ptr->size - 1)
@@ -31,7 +32,7 @@ int isFull(struct stack *ptr)
         return 0;
     }
 }
-
+ 
 void push(struct stack* ptr, char val){
     if(isFull(ptr)){
         printf("Stack Overflow! Cannot push %d to the stack\n", val);
@@ -41,7 +42,7 @@ void push(struct stack* ptr, char val){
         ptr->arr[ptr->top] = val;
     }
 }
-
+ 
 char pop(struct stack* ptr){
     if(isEmpty(ptr)){
         printf("Stack Underflow! Cannot pop from the stack\n");
@@ -53,63 +54,65 @@ char pop(struct stack* ptr){
         return val;
     }
 }
-
+ 
+char stackTop(struct stack* sp){
+    return sp->arr[sp->top];
+}
+ 
+int match(char a, char b){
+    if(a=='{' && b=='}'){
+        return 1;
+    }
+    if(a=='(' && b==')'){
+        return 1;
+    }
+    if(a=='[' && b==']'){
+        return 1;
+    }
+  return 0;  
+}
+ 
 int parenthesisMatch(char * exp){
     // Create and initialize the stack
-    struct stack* sp=(struct stack * ) malloc (sizeof(struct stack));
+    struct stack* sp;
     sp->size = 100;
     sp->top = -1;
     sp->arr = (char *)malloc(sp->size * sizeof(char));
-
-
+    char popped_ch;
+ 
     for (int i = 0; exp[i]!='\0'; i++)
     {
-        if(exp[i]=='('){
-            push(sp, '(');
+        if(exp[i]=='(' || exp[i]=='{' || exp[i]=='['){
+            push(sp, exp[i]);
         }
-        else if(exp[i]==')'){
+        else if(exp[i]==')'|| exp[i]=='}' || exp[i]==']'){
             if(isEmpty(sp)){
                 return 0;
             }
-            pop(sp);
-        }
-        if(exp[i]=='{'){
-            push(sp, '{');
-        }
-        else if(exp[i]=='}'){
-            if(isEmpty(sp)){
-                return 0;
+            popped_ch = pop(sp);
+            if(!match(popped_ch, exp[i])){ 
+              return 0;  
             }
-            pop(sp);
-        }
-        if(exp[i]=='['){
-            push(sp, '[');
-        }
-        else if(exp[i]==']'){
-            if(isEmpty(sp)){
-                return 0;
-            }
-            pop(sp);
         }
     }
-
+ 
     if(isEmpty(sp)){
         return 1;
     }
     else{
         return 0;
     }
-
+    
 }
+ 
 int main()
 {
-    char * exp = "((8)[{}(*--$$9))";
-    // Check if stack is empty
+    char * exp = "({})";
     if(parenthesisMatch(exp)){
-        printf("The parenthesis is matching");
+        printf("The parenthesis is balanced");
     }
     else{
-        printf("The parenthesis is not matching");
+        printf("The parenthesis is not balanced");
     }
     return 0;
 }
